4a652145d17eb92e58c713860ee708b919499a8f
Gilberto Cardoso <gilberto.cardoso537@gmail.com>
2020-01-25 13:57:06 -0300

    Testando a primeira classe da categoria

--- src/main/java/com/nelioalves/cursomc/controller/CategoriaController.java
+++ src/main/java/com/nelioalves/cursomc/controller/CategoriaController.java
@@ -1,15 +1,28 @@
 package com.nelioalves.cursomc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.model.categoria;
 
 @RestController	
 @RequestMapping(value="/categorias")
 public class CategoriaController {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-	  return "Rest esta funcionando!";
+	public List<categoria> listar() {
+		
+	  categoria cat1 = new categoria(1,"Informatica");
+	  categoria cat2 = new categoria(2,"Escritorio");
+	  
+	  List<categoria> lista = new ArrayList<>();
+	  lista.add(cat1);
+	  lista.add(cat2);
+	  
+	  return lista;
 	}
 }
