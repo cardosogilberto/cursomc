33db2c6a7e53f13f75ee10f9b26232782d9c19a5
gilberto.cardoso537@gmail.com <gilberto.cardoso537@gmail.com>
2020-02-15 21:34:20 -0300

    correcao

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -123,11 +123,11 @@ public class DBService {
 			estadoRepository.saveAll(Arrays.asList(est1,est2));
 			cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 			
-			Cliente cli1 = new Cliente(null, "Maria Silva Borges", "maria@gmail.com", "70758641060", TipoCliente.PESSOAFISICA, pe.encode("123"));
-
+			Cliente cli1 = new Cliente(null, "Maria Silva", "nelio.cursos1@gmail.com", "21258543508", TipoCliente.PESSOAFISICA, pe.encode("123"));
+			
 			cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 			
-			Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+			Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm1@gmail.com", "67748746737", TipoCliente.PESSOAFISICA, pe.encode("123"));
 			cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
 			cli2.addPerfil(Perfil.ADMIN);
 
